3a2cf60b99b472f77bc02c1c1ba9aaa45cc956e9
gaitou2048 <gaitou2048@gmail.com>
2023-01-30 22:29:54 +0900

    Add: バックエンドから値を取得するサンプルを追加

--- front/app/vite.config.ts
+++ front/app/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api":{
-        target: "http://back/5000",
+        target: "http://back:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
